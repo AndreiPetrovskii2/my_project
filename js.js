422643144d50407798cfb3dd04f9c5ca7b04fb77
andrei petrovsky <andrei_2001pet@mail.ru>
2024-02-09 01:32:38 +0500

    second commit fix

--- js.js
+++ js.js
@@ -2,8 +2,19 @@ var donate = document.getElementById('donate');
 var money = document.getElementById('money_donate');
 var body = document.getElementById('dark');
 var header = document.getElementById('header');
+let img_escape = document.getElementById('img_escape');
+let input = document.querySelector('input');
+let button_donate = document.getElementById('button_donate');
 donate.addEventListener('click',function(){
     money.style ='display:block;position: fixed;';
-    body.style = 'background-color: #8C92AC;opacity: 30%;';
-    header.style = 'background-color: #8C92AC;opacity: 30%;';
+    body.style = 'background-color: #8C92AC;opacity: 5%;';
+    header.style = 'background-color: #8C92AC;opacity: 5%;';
+})
+img_escape.addEventListener('click', function(){
+    money.style ='display:none;';
+    body.style = 'opacity: 100%;';
+    header.style = 'opacity: 100%;';
+})
+button_donate.addEventListener('click', function(){
+    alert('Спасибо что перечислили ' + input.value + ' руб')
 })
